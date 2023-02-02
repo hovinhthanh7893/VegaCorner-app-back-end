bb8bde04d29b9bff12a3a132fb1b8ed7bfcef164
hovinhthanh7893 <hovinhthanh7893@gmail.com>
2023-02-02 12:34:56 +0100

    change

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-> pip3 install "fastapi[all]"
+## Set up a Virtual Environment with Pipenv
+
+> pip install --user pipenv
+
+(for window)
+
+> py -m pip install pipenv
+
+## Install package
+
+> pipenv install "fastapi"
+> pipenv install "uvicorn[standard]"
+> pipenv install pydantic
+
+## Run server
 
 > uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,35 +1,11 @@
+# Import FastAPI
 from fastapi import FastAPI
-from pydantic import BaseModel
 
+# Create app
 app = FastAPI()
 
-class Recipe(BaseModel):
-    name: str
-    ingredient: str
-
-inventory = {
-    1: {
-        "name": "Tofu",
-        "ingredient": "Soy bean"
-    }
-}
-
+# Create end-point
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/recipe_id/{id}")
-def get_item(id: int):
-    return inventory[id]
-
-@app.get("/recipe_name/{name}")
-def get_item(name: str):
-    for id in inventory:
-        if inventory[id]["name"] == name:
-            return inventory[id]
-    return {"Data": "Not found"}
-
-@app.post("/create_recipe")
-def create_item(recipe: Recipe):
-    inventory[len(inventory) + 1] = {"name": recipe.name, "ingredient": recipe.ingredient}
-    return inventory[len(inventory) + 1] 
